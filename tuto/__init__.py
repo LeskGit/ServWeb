271a27f9746cc442e2789c625a5465456776df30
LeskGit <hacksel002@gmail.com>
2024-09-17 14:56:23 +0200

    TP2 entier terminé

--- tuto/__init__.py
+++ tuto/__init__.py
@@ -1,2 +1,5 @@
 from .app import app
 import tuto.views
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy (app)
--- tuto/views.py
+++ tuto/views.py
@@ -1,6 +1,34 @@
 from .app import app
+from flask import render_template
+from .models import get_sample
 
 @app.route ("/")
-
-def home ():
-    return "<h1 > Hello World </h1 >"
+def home():
+    return render_template(
+        "home.html",
+        title="Hello World")
+    
+    
+@app.route ("/name")
+def show_name():
+    return render_template(
+        "home.html",
+        title="name",
+        names=["Pierre", "Jean", "Axel"])
+    
+    
+@app.route ("/sample")
+def sample():
+    return render_template(
+        "home.html",
+        title="My Books !",
+        books=get_sample())
+    
+    
+@app.route("/detail/<id>")
+def detail(id):
+    books = get_sample()
+    book = books[int(id)]
+    return render_template(
+    "detail.html",
+    b=book)
